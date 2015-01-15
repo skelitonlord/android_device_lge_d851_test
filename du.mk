cf0e61e7c93f5e97d1d20ad25519d54c29cbeeb0
Daniel Saporita <dajasa22793@gmail.com>
2015-01-15 12:22:13 -0500

    Update du.mk
    some more changes to add DU support

--- du.mk
+++ du.mk
@@ -1,11 +1,14 @@
 $(call inherit-product, device/lge/d851/full_d851.mk)
-
-# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+# Boot animation
+TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2560
+
+PRODUCT_RELEASE_NAME := LG G3
 PRODUCT_NAME := du_d851
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_tmo_us" BUILD_FINGERPRINT="lge/g3_tmo_us/g3:4.4.2/KVT49L.D85110c/D85110c.1403267110:user/release-keys" PRIVATE_BUILD_DESC="g3_tmo_us-user 4.4.2 KVT49L.D85110c D85110c.1403267110 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_DEVICE="du_d851" \
+PRODUCT_NAME="g3_tmo_us"
